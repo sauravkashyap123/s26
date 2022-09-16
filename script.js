302eaed1296f2069e92445d1bc230de26c5703a3
sauravkashyap123 <113660773+sauravkashyap123@users.noreply.github.com>
2022-09-16 11:47:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "amisha" && b=="17-09-2003")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
