556da8454c715f2b60c7168b98823269e4e86103
sauravkashyap123 <113660773+sauravkashyap123@users.noreply.github.com>
2022-11-08 12:11:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a== "amisha" && b=="17-09-2002")
+  	if(a== "shubham" && b=="17-09-2002")
   	{
   	    window.location.href="start.html";
   	}
